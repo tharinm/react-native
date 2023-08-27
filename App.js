6a2e18e4b997c870281984ea96a1e3d732efe7d6
tharinm <tharindumadhushan864@gmail.com>
2023-08-27 18:05:52 +0530

    flex layout

--- App.js
+++ App.js
@@ -3,18 +3,33 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <View> 
-        <TextInput placeholder="Your Goals"/>
-      <Button title="Add Goal"/>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your Goals" style={styles.textInput} />
+        <Button title="Add Goal" />
       </View>
       <View>
-        <Text>
-          List of Goals
-        </Text>
+        <Text>List of Goals</Text>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+ 
+  },
+textInput:{
+  borderWidth:1,
+  borderColor:'gray',
+  // borderRadius:6,
+  width:'80%',
+  padding:10,
+  marginRight:12
+}
+});
